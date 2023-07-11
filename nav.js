e7d2faf5c0aff174c7b80b5a0781a4ac1fed38b6
aashishrana <aashishrana71135@gmail.com>
2023-07-10 23:45:29 -0700

    Navigation

---
+++ nav.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', function() {
+  menu.classList.toggle('active');
+});
